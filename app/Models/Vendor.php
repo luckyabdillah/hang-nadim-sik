f168c043eaf685c3ac6a60cc30301bd810053264
Ziva Dasfi Sadira <zivadasfi@gmail.com>
2025-04-18 21:11:43 +0700

    Feat: model and controller setup for Vendor CRUD

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Vendor extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'addres',
+    ];
+    
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating (function ($model) {
+            if (empty($model->uuid)){
+                $model->uuid = Str::uuid()->toString();
+            }
+        });
+    }
 }
